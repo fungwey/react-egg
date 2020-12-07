9774cf09dd77eb3a58614146c884407a04f63f3f
wittewey <549014963@qq.com>
2020-12-07 17:06:39 +0800

    特性: ejs 模版引擎中静态资源的使用和配置

--- egg/config/config.default.js
+++ egg/config/config.default.js
@@ -36,6 +36,11 @@ module.exports = (appInfo) => {
     delimiter: '%',
   };
 
+  config.static = {
+    prefix: "/assets/",
+    dir: path.join(appInfo.baseDir, "app/assets"),
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
