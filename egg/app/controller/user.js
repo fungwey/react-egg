eb397e8a2d993d324eb5b33ae253a75670ebc593
wittewey <549014963@qq.com>
2020-12-07 17:37:25 +0800

    特性: egg.js 中 Cookie 的配置和使用以及如何设置中文cookie

--- egg/app/controller/user.js
+++ egg/app/controller/user.js
@@ -3,15 +3,48 @@
 const Controller = require("egg").Controller;
 
 class UserController extends Controller {
+  encode(str = '') {
+    return new Buffer(str).toString("base64");
+  }
+  decode(str = '') {
+    return new Buffer(str, "base64").toString();
+  }
   async index() {
     const { ctx } = this;
     // ctx.body = "user index";
+    // 获取session
+    const session = ctx.session.user;
+    const zhSession = ctx.session.zh;
+    console.log(session);
+    console.log(zhSession);
+
+    /**
+     * 不能设置中文
+     * 如果需要设置中文cookie需要加密
+     */
+    ctx.cookies.set("zh", "测试", {
+      encrypt: true,
+    });
+    // 获取中文的需要解密
+    const zh = ctx.cookies.get("zh", {
+      encrypt: true,
+    });
+    // console.log(zh)
+
+    ctx.cookies.set("base64", this.encode("中文base64"));
+    const base64 = this.decode(ctx.cookies.get("base64"));
+
+    // ctx.body = 'user index';
+    const user = ctx.cookies.get("user");
     await ctx.render('user.html', {
       id: 100,
       name: 'admin',
       lists: [
         'java', 'php', 'ts',
       ],
+      user: user ? JSON.parse(user) : null,
+      zh,
+      base64,
     }, {
       delimiter: '%',
     });
@@ -68,6 +101,37 @@ class UserController extends Controller {
     ctx.body = ctx.request.body.id;
   }
 
+
+  async login() {
+    const { ctx } = this;
+    const body = ctx.request.body;
+    ctx.cookies.set("user", JSON.stringify(body), {
+      maxAge: 1000 * 60 * 10,
+      httpOnly: false, // 是否允许浏览器修改
+    });
+
+    // 保存session
+    ctx.session.user = body;
+    ctx.session.zh = "中文测试";
+    ctx.session.test = "test";
+
+    ctx.body = {
+      status: 200,
+      data: body,
+    };
+  }
+
+  async logout() {
+    const { ctx } = this;
+    ctx.cookies.set("user", null);
+
+    // 清除session
+    ctx.session.user = null;
+
+    ctx.body = {
+      status: 200,
+    };
+  }
 }
 
 module.exports = UserController;
